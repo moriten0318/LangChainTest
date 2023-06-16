719ce3189af562342edba7088efe674735714cf5
DESKTOP-GGBBF12\moris <morishi0318@outlook.jp>
2023-06-17 00:23:31 +0900

    LLMが使えるかのテスト（使えた！やったね！）

--- LangChainTest01.py
+++ LangChainTest01.py
@@ -1,7 +1,14 @@
 import os
+from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.environ['OPENAI_API_KEY']
-print(api_key)
+print('myAPIkey='+api_key)
+
+llm=OpenAI(model_name="text-davinci-003",openai_api_key=api_key)
+#openAIインスタンスを作るときはopenai_api_key変数にAPIキーを渡す必要あり
+print("ハロー？")
+response=llm("教育学的な視点から教育をすることの意義を説明する")
+print(response.score)
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
+from langchain.llms import OpenAI
+
+llm=OpenAI(model_name="text-davinci-003")
+llm("大阪市の人口について説明する")
 print("HelloWorld")
